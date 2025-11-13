35d674e5df2539dbac12e9eb20a5695762616f0e
Adi4669 <adityachitgopker@gmail.com>
2025-11-13 11:52:03 +0530

    Refactor supplier.sql for improved queries and commands
    Updated SQL queries for consistency and added SHOW commands.

--- supplier.sql
+++ supplier.sql
@@ -1,5 +1,6 @@
-create database supplier;
-use supplier;
+SHOW DATABASES;
+USE SUPPLIER;
+SHOW TABLES;
 
 create table supplier(sid int primary key, sname varchar(255), city varchar(255));
 desc supplier;
@@ -31,28 +32,24 @@ insert into catalog values
 (10003, 20003, 30),
 (10004, 20003, 40);
 
-select * from supplier;
-select * from parts;
-select * from catalog;
+SELECT * FROM CATALOG;
+SELECT * FROM PARTS;
+SELECT * FROM SUPPLIER;
 
 -- query 3
 select distinct p.pname
 from parts p
-join catalog c
-on p.pid = c.pid;
+join catalog c on p.pid = c.pid;
 
 -- query 4
-select s.sname
-from supplier s
-join catalog c
-on s.sid = c.sid
+select s.sname from supplier s
+join catalog c on s.sid = c.sid
 group by s.sid, s.sname
 having count(distinct c.pid) = (select count(*) from parts);
 
 -- query 5
-select s.sname as supplier_supplying_all_red_parts
-from supplier s
-join catalog c on s.sid = c.sid
+select s.sname from supplier s
+join catalog c on  s.sid = c.sid
 join parts p on c.pid = p.pid
 where p.color = "red"
 group by s.sid
@@ -63,4 +60,16 @@ select p.pname from parts p
 join catalog c on p.pid = c.pid
 group by p.pid, p.pname
 having count(distinct c.sid) = 1
-and max(c.sid) = (select sid from supplier where sname = "Acme Widget");
+and max(c.sid) = (select sid from supplier where sname = "acame widget");
+
+-- query 7
+select distinct c.sid, c1.avg_cost from catalog c
+join (select pid, avg(cost) as avg_cost from catalog group by pid)
+c1 on c.pid = c1.pid
+where c.cost > c1.avg_cost;
+
+-- query 8
+select p.pid, s.sname, c.cost from catalog c
+join supplier s on c.sid = s.sid
+join parts p on c.pid = p.pid
+where (c.pid, c.cost) in (select pid, max(cost) from catalog group by pid);
